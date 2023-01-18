bcac8b54d7db5b92361d86d56617c3a8e2101333
Andrew Panferov <panferov.av@gmail.com>
2023-01-18 19:43:51 +0300

    Выполненное задание.

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,15 @@
 package homework;
 
+import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-
-    //todo: 2. надо реализовать методы этого класса
-    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+    private final LinkedList<Customer> holder = new LinkedList<>();
 
     public void add(Customer customer) {
-
+        holder.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return holder.removeLast();
     }
 }
--- L04-generics/src/main/java/homework/CustomerService.java
+++ L04-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,29 @@
 package homework;
 
-
+import java.util.AbstractMap;
+import java.util.Comparator;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
-
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> holder =
+            new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return getSimpleEntryWithCloneKey(holder.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return getSimpleEntryWithCloneKey(holder.higherEntry(customer));
+    }
+
+    private Map.Entry<Customer, String> getSimpleEntryWithCloneKey(Map.Entry<Customer, String> entry) {
+        return entry == null
+                ? null
+                : new AbstractMap.SimpleEntry<>(entry.getKey().clone(), entry.getValue());
     }
 
     public void add(Customer customer, String data) {
-
+        holder.put(customer, data);
     }
 }
