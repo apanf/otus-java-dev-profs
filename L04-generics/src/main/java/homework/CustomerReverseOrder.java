edba4efdd304090ecd028f3a55d01473cfc00c49
Andrew Panferov <panferov.av@gmail.com>
2023-01-19 19:05:21 +0300

    Исправление по замечанию.

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,9 +1,10 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    private final LinkedList<Customer> holder = new LinkedList<>();
+    private final Deque<Customer> holder = new LinkedList<>();
 
     public void add(Customer customer) {
         holder.add(customer);
