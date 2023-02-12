93b768e90c8258f024fba616165d6b7e8fac62f6
Andrew Panferov <panferov.av@gmail.com>
2023-02-12 11:30:36 +0300

    Добавил тесты.

---
+++ L10-byteCodes/src/test/java/homework/AnnotationHandlersTest.java
@@ -0,0 +1,66 @@
+package homework;
+
+import homework.annotations.AnnotationHandler;
+import homework.annotations.Log;
+import homework.annotations.LogAnnotationHandler;
+import homework.annotations.Meter;
+import homework.annotations.MeterAnnotationHandler;
+import homework.annotations.Save;
+import homework.annotations.SaveAnnotationHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SuppressWarnings("unchecked")
+@ExtendWith(MockitoExtension.class)
+public class AnnotationHandlersTest {
+    @Spy
+    private LogAnnotationHandler logAnnotationHandler;
+    @Spy
+    private SaveAnnotationHandler saveAnnotationHandler;
+    @Spy
+    private MeterAnnotationHandler meterAnnotationHandler;
+    private Ioc ioc;
+
+    @BeforeEach
+    public void setUp() {
+        Map<Class<? extends Annotation>, AnnotationHandler> annotationHandlers = new HashMap<>();
+        annotationHandlers.put(Log.class, logAnnotationHandler);
+        annotationHandlers.put(Save.class, saveAnnotationHandler);
+        annotationHandlers.put(Meter.class, meterAnnotationHandler);
+
+        ioc = new Ioc(annotationHandlers);
+    }
+
+    @Test
+    @DisplayName("Проверка работы аннотаций с фазой вызова BEFORE")
+    public void test1() {
+        UnitOfWork<String> work = (UnitOfWork<String>) ioc.createClass(UnitOfWorkImpl.class);
+
+        work.doWork(1, 2, "1", "2", "3");
+
+        verify(logAnnotationHandler).handle(any());
+        verify(saveAnnotationHandler).handle(any());
+    }
+
+    @Test
+    @DisplayName("Проверка работы аннотаций с фазой вызова AROUND")
+    public void test2() {
+        UnitOfWork<String> work = (UnitOfWork<String>) ioc.createClass(UnitOfWorkImpl.class);
+
+        work.doWork(1, 2, "1", "2", "3");
+
+        verify(meterAnnotationHandler, times(2)).handle(any());
+    }
+}
---
+++ L10-byteCodes/src/test/java/homework/IocTest.java
@@ -0,0 +1,59 @@
+package homework;
+
+import homework.annotations.AnnotationHandler;
+import homework.annotations.Log;
+import homework.annotations.LogAnnotationHandler;
+import homework.annotations.Meter;
+import homework.annotations.MeterAnnotationHandler;
+import homework.annotations.Save;
+import homework.annotations.SaveAnnotationHandler;
+import homework.testClasses.UnitOfWorkDouble;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SuppressWarnings("unchecked")
+public class IocTest {
+    private Ioc ioc;
+
+    @BeforeEach
+    public void setUp() {
+        Map<Class<? extends Annotation>, AnnotationHandler> annotationHandlers = new HashMap<>();
+        annotationHandlers.put(Log.class, new LogAnnotationHandler());
+        annotationHandlers.put(Meter.class, new MeterAnnotationHandler());
+        annotationHandlers.put(Save.class, new SaveAnnotationHandler());
+
+        ioc = new Ioc(annotationHandlers);
+    }
+
+    @Test
+    @DisplayName("Проверка, что для одного и того же класса используется 1 обработчик")
+    public void test1() {
+        UnitOfWork<String> work = (UnitOfWork<String>) ioc.createClass(UnitOfWorkImpl.class);
+
+        work.doWork();
+        work.doWork("test");
+
+        assertEquals(1, ioc.getHandlers().size());
+    }
+
+    @Test
+    @DisplayName("Проверка, что для разных классов, используются разные обработчики")
+    public void test2() {
+        UnitOfWork<String> work1 = (UnitOfWork<String>) ioc.createClass(UnitOfWorkImpl.class);
+        UnitOfWork<Double> work2 = (UnitOfWork<Double>) ioc.createClass(UnitOfWorkDouble.class);
+
+        work1.doWork();
+        work1.doWork("test");
+        work2.doWork(1.2, 2.4, 3.1, 4.4, 5.4, 6.0, 0.7);
+        work1.doWork();
+
+        assertEquals(2, ioc.getHandlers().size());
+    }
+}
